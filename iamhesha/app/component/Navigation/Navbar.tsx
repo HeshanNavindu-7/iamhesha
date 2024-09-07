d49dc88dcd02d94098bcd20fb1d406ea5e7a7da9
Heshan Navindu <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-09-07 22:24:32 +0530

    app update

---
+++ iamhesha/app/component/Navigation/Navbar.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import { Cover } from "../ui/cover";
+import { HoverBorderGradient } from "../ui/hover-border-gradient";
+
+const Navbar = () => {
+  const leftLinks = [
+    { id: 1, link: "Home", href: "/" },
+    { id: 2, link: "About", href: "/about" },
+    { id: 3, link: "Services", href: "/services" },
+  ];
+
+  const rightLinks = [
+    { id: 3, link: "Skills", href: "/skills" },
+    { id: 4, link: "Blog", href: "/blog" },
+    { id: 5, link: "Contact", href: "/contact" },
+  ];
+
+  return (
+    <>
+         <HoverBorderGradient
+        containerClassName="rounded-full"
+        as="button"
+        className="dark:bg-black bg-gray-800 text-black dark:text-white flex items-center space-x-2"
+      >
+    <div className="bg-gray-800 text-white rounded-3xl w-full h-16 flex justify-between items-center px-4">
+      <ul className="mx-24 flex space-x-24">
+        {leftLinks.map(({ id, link, href }) => (
+          <li key={id} className="cursor-pointer hover:text-yellow-400">
+            <Link href={href}>{link}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className="flex-1 flex justify-center">
+        <Link href="/">
+        <div>
+      <h1 className="text-2xl font-bold  ">
+        <Cover>iamHesha</Cover>
+      </h1>
+    </div>
+        </Link>
+      </div>
+
+      <ul className=" mx-24 flex space-x-24">
+        {rightLinks.map(({ id, link, href }) => (
+          <li key={id} className="cursor-pointer hover:text-yellow-400">
+            <Link href={href}>{link}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+    </HoverBorderGradient>
+    </>
+  );
+};
+
+export default Navbar;
--- iamhesha/app/page.tsx
+++ iamhesha/app/page.tsx
@@ -1,101 +1,68 @@
+'use client'
 import Image from "next/image";
+import Navbar from "./component/Navigation/Navbar";
+import { FlipWords } from "./component/ui/flip-words";
+import { Vortex } from "./component/ui/vortex";
 
 export default function Home() {
+  const words = ["better", "cute", "beautiful", "modern"];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <Vortex
+        backgroundColor="black"
+        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+      >
+      <Navbar/>
+      <div className="flex justify-between items-center py-20 px-10  text-white relative">
+    <div className="max-w-xl">
+      <h1 className="text-5xl font-bold mb-6">
+        I'm <span className="text-cyan-400">Heshan Navindu</span>,<br />
+        <FlipWords words={words} /> <br />
+      </h1>
+      <div className="flex gap-4 mb-6">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          className="px-6 py-2 rounded-full bg-cyan-400 text-white font-semibold"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          Download CV
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          className="px-6 py-2 rounded-full bg-orange-400 text-white font-semibold"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          Hire me
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+      </div>
+      <div className="flex gap-4">
+        <a href="#" className="text-white text-xl">
+          <i className="fab fa-facebook-f"></i>
         </a>
-      </footer>
+        <a href="#" className="text-white text-xl">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="#" className="text-white text-xl">
+          <i className="fab fa-instagram"></i>
+        </a>
+        <a href="#" className="text-white text-xl">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
+        <a href="#" className="text-white text-xl">
+          <i className="fab fa-github"></i>
+        </a>
+      </div>
     </div>
+    <div className="">
+      <Image
+        src="/images/iamhesha.png"
+        alt="Heshan Navindu"
+        width={500}
+        height={500}
+        className="rounded-lg"
+      />
+      <div className="absolute bottom-0 right-0 w-96 h-96 bg-cyan-400 rounded-full z-[-1]"></div>
+    </div>
+  </div>
+  </Vortex>
+    </main>
   );
 }
