f9806c7d16ad986047b4856cdb2847ea6d629522
Heshan Navindu <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-09-23 01:19:34 +0530

    cv add

--- iamhesha/app/Pages/projects/page.tsx
+++ iamhesha/app/Pages/projects/page.tsx
@@ -1,16 +1,18 @@
 import Navbar from "@/app/component/Navigation/Navbar";
 import { ExpandableCardDemo } from "@/app/component/section/card/ExpandableCardDemo";
+import Footer from "@/app/component/section/Footer/Footer";
 
 export default function projects(){
   return (
-<>
-<div>
+<div className="flex min-h-screen flex-col items-center justify-between bg-black">
+<div className="w-full flex justify-center mt-8">
 <Navbar/>
 </div>
 <div>
 <h2 className="text-4xl font-bold mb-6 text-center">Projects</h2>
 <ExpandableCardDemo/>
 </div>
-</>
+<Footer/>
+</div>
   )
 }
